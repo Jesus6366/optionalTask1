8dd225af35c566972a05a1b658060d94544e51a5
Jesus Jose Martha Laborin <89470786+Jesus6366@users.noreply.github.com>
2024-09-07 17:04:01 -0600

    Create Task.java

---
+++ Task.java
@@ -0,0 +1,11 @@
+// JavaScript code:
+console.log("Itransition");
+
+// Java code:
+/*
+public class Task {
+    public static void main(String[] args) {
+        System.out.println("Itransition");
+    }
+}
+*/
